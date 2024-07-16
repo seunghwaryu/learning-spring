2f956a2131f210c8455932d11a5a8f0a77eb6600
Seunghwa Ryu <rshh0714@gmail.com>
2024-07-16 13:10:09 +0900

    회원 웹 기능 - 등록

--- src/main/java/learning/learning_spring/controller/MemberController.java
+++ src/main/java/learning/learning_spring/controller/MemberController.java
@@ -1,9 +1,14 @@
 package learning.learning_spring.controller;
 
+import learning.learning_spring.domain.Member;
 import learning.learning_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -15,8 +20,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping
+    @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList.html";
     }
 }
