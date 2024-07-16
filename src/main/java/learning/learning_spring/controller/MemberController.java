cb8c7fd97c5629d3c06f6813ee54e4c157bc22fb
Seunghwa Ryu <rshh0714@gmail.com>
2024-07-16 13:00:38 +0900

    회원 관리용 홈 추가

--- src/main/java/learning/learning_spring/controller/MemberController.java
+++ src/main/java/learning/learning_spring/controller/MemberController.java
@@ -3,6 +3,7 @@ package learning.learning_spring.controller;
 import learning.learning_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +13,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
