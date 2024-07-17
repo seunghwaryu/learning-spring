7a0fec86595ce3baa47b9433ab2818f97a5b9a84
Seunghwa Ryu <rshh0714@gmail.com>
2024-07-17 16:36:43 +0900

    순수 Jdbc 사용해서 DB 접근

--- src/main/java/learning/learning_spring/SpringConfig.java
+++ src/main/java/learning/learning_spring/SpringConfig.java
@@ -1,13 +1,21 @@
 package learning.learning_spring;
 
+import learning.learning_spring.repository.JdbcMemberRepository;
 import learning.learning_spring.repository.MemberRepository;
-import learning.learning_spring.repository.MemoryMemberRepository;
 import learning.learning_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
